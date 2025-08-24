edac61b7dbb9cfffc92c8808dfafee3cd706a3ed
badisjl99 <badisjlassi86@gmail.com>
2025-08-24 03:00:37 +0100

    v1.0.8

--- backend/server/routes/auth.js
+++ backend/server/routes/auth.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 const { generateToken, verifyToken ,authenticateToken} = require('../utils/jwt');
 
 
-/*
+
 router.post('/register', async (req, res, next) => {
   const { fullName, email, password, role } = req.body;
 
@@ -39,7 +39,7 @@ router.post('/register', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});*/
+});
 
 router.post('/login', async (req, res, next) => {
   const { email, password } = req.body;
