831da16f575279a9c66c5eca39a1b2187d9a8ef8
badisjl99 <badisjlassi86@gmail.com>
2025-08-24 23:56:54 +0100

    v3.1.0

--- mongo-init.js
+++ mongo-init.js
@@ -1,12 +1,10 @@
 // /mongo-init.js
 
-// Switch to the application database. MongoDB will create it if it doesn't exist.
 db = db.getSiblingDB('mydb');
 
-// Create a non-root user with read/write permissions for the application
 db.createUser({
   user: 'badis',
-  pwd: '123321@00Pkdz?;fde07', // This password must match MONGO_APP_PASSWORD in your .env file
+  pwd: '123321@00Pk.3dz?fde07',
   roles: [
     {
       role: 'readWrite',
